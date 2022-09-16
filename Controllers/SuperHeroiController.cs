426b1f3280d0e6e74fb050700e720376f6dfd50c
Moisés Sansigolo <moises.sansigolo@gmail.com>
2022-09-16 14:06:01 -0300

    Adicionar arquivos de projeto.

---
+++ Controllers/SuperHeroiController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SuperHeroisApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperHeroisApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SuperHeroiController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public SuperHeroiController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<SuperHeroi>>> Get()
+        {
+            return Ok(await _context.SuperHerois.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<SuperHeroi>> GetById(int id)
+        {
+            var heroi = await _context.SuperHerois.FindAsync(id);
+            if (heroi == null)
+            {
+                return BadRequest("Heroi não encontrado");
+            }
+            return Ok(heroi);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<SuperHeroi>>> AddHero(SuperHeroi heroi)
+        {
+            _context.SuperHerois.Add(heroi);            
+            await _context.SaveChangesAsync();
+
+            return Ok("Heroi criado com sucesso");
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<List<SuperHeroi>>> UpdateHero(SuperHeroi request)
+        {
+            var heroi = await _context.SuperHerois.FindAsync(request.Id);
+            if (heroi == null)
+
+                return BadRequest("Heroi não encontrado");
+
+            heroi.Name = request.Name;
+            heroi.FirstName = request.FirstName;
+            heroi.LastNmae = request.LastNmae;
+            heroi.Place = request.Place;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(heroi);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<SuperHeroi>>> Delete(int id)
+        {
+            var heroi = await _context.SuperHerois.FindAsync(id);
+            if (heroi == null)
+                return BadRequest("Heroi não encontrado");
+
+            _context.SuperHerois.Remove(heroi);
+            await _context.SaveChangesAsync();
+            return Ok("Heroi excluido com sucesso.");
+        }
+    }
+}
---
+++ Migrations/20220916162153_criando banco.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SuperHeroisApi.Migrations
+{
+    public partial class criandobanco : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SuperHerois",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    LastNmae = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Place = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SuperHerois", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SuperHerois");
+        }
+    }
+}
